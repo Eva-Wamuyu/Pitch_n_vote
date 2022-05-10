039dd21aa8a011ad6e31ff9a9b52bfbbbec10342
Eva <rutheve.eva@gmail.com>
2022-05-10 15:04:16 +0300

    add instance of mail

--- app/__init__.py
+++ app/__init__.py
@@ -1,24 +1,38 @@
 from flask import Flask
-# from ..config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
+from flask_login import LoginManager
+from flask_mail import Mail
+
+from config import config_options
 
 db = SQLAlchemy()
 bootstrap = Bootstrap()
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+mail = Mail()
+
+
 
 #MAIN ENTRY OF THE APP
-def create_app():
+def create_app(config_filename):
     app = Flask(__name__)
-
     
     
-    # app.config.from_object(config_options[config_filename])
+    
+    app.config.from_object(config_options[config_filename])
     db.init_app(app)
     bootstrap.init_app(app)
-    from main import app_bp
+    login_manager.init_app(app)
+    mail.init_app(app)
+    from .main import app_bp
     app.register_blueprint(app_bp)
+    
+   
+
+
     
     return app
 
 
-create_app()
+
