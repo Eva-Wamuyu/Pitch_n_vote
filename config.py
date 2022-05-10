9d55d8847a34f85b86e1a9b7d1172c17cab1b90c
Eva <rutheve.eva@gmail.com>
2022-05-10 18:46:08 +0300

    add configs and files to ignore

--- config.py
+++ config.py
@@ -1,10 +1,23 @@
 import os
-class Config():
-    SECRET_KEY = os.environ['SECRET_KEY']
-    USER_NAME = os.environ['USER_NAME']
-    DATABASE = os.environ['DATABASE']
-    HOSTNAME = os.environ['HOSTNAME']
 
+
+class Config():
+    
+    
+    SECRET_KEY = os.environ['SECRET_KEY']
+    HOSTNAME = os.environ['HOSTNAME']
+    DBUSERNAME = os.environ['DBUSERNAME']
+    DBPASSWORD = os.environ['DBPASSWORD']
+    DBNAME = os.environ['DBNAME']
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{}:{}@localhost/{}'.format(DBUSERNAME,DBPASSWORD,DBNAME)
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
+
+    EMAIL_SERVER = 'smtp.gmail.com'
+    EMAIL_PORT = 587
+    EMAIL_USE_TSL = True
+    USER_NAME = os.environ['USER_NAME']
+    USER_PASS = os.environ['USER_PASS']
 
 
 
@@ -18,7 +31,9 @@ class DevConfig(Config):
 
 
 class ProdConfig(Config):
-  pass
+  SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+  
 
 config_options = {
   'dev': DevConfig,
